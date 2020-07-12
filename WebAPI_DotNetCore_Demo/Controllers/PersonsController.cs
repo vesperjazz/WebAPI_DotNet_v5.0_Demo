4e96ccb23ee25064cb127d1778698b87d6f4d739
Ivan Chin <ivanchin91@hotmail.com>
2020-07-12 23:25:34 +0800

    - Initial project cleanup from Microsoft template.

---
+++ WebAPI_DotNetCore_Demo/Controllers/PersonsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebAPI_DotNetCore_Demo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonsController
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> GetAllAsync(CancellationToken cancellationToken)
+        {
+            return new List<string> { "Ivan Chin", "Aragorn Elessar", "Arwen Undomiel", "Gandalf Greyhame" };
+        }
+
+        [HttpGet("{ID}")]
+        public async Task<ActionResult<string>> GetByIDAsync(Guid ID, CancellationToken cancellationToken)
+        {
+            return "Ivan Chin";
+        }
+    }
+}
