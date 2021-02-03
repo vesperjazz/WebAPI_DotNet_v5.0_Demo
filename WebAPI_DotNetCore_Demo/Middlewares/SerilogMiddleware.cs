5fca0cfc5415a79775f02237cdcf7f70251a50e9
Ivan Chin <ivanchin91@hotmail.com>
2021-02-03 22:33:41 +0800

    - Fixed middleware (singleton) containing state information which logs the wrong request/response bodies when concurrent request occurs.

--- WebAPI_DotNetCore_Demo/Middlewares/SerilogMiddleware.cs
+++ WebAPI_DotNetCore_Demo/Middlewares/SerilogMiddleware.cs
@@ -18,8 +18,6 @@ namespace WebAPI_DotNetCore_Demo.Middlewares
     {
         private const string MessageTemplate = "{RequestScheme} {RequestMethod} {RequestPath} responded {StatusCode} in {Elapsed:0.0000} ms";
 
-        private string _requestBody;
-        private string _responseBody;
         private readonly RequestDelegate _next;
 
         public SerilogMiddleware(RequestDelegate next)
@@ -32,29 +30,26 @@ namespace WebAPI_DotNetCore_Demo.Middlewares
             if (httpContext is null) { throw new ArgumentNullException(nameof(httpContext)); }
 
             var start = Stopwatch.GetTimestamp();
-            var responseBody = new MemoryStream();
+            var responseBodyStream = new MemoryStream();
             var originalResponseBodyStream = httpContext.Response.Body;
+            var requestBody = await GetBodyAsync(httpContext.Request);
 
             try
             {
-                _requestBody = await GetBodyAsync(httpContext.Request);
-                // Replaces the content with null if empty or whitespace, so that null will be persisted into database.
-                _requestBody = string.IsNullOrWhiteSpace(_requestBody) ? null : _requestBody;
-
-                httpContext.Response.Body = responseBody;
+                httpContext.Response.Body = responseBodyStream;
 
                 await _next(httpContext);
 
                 var elapsedMs = GetElapsedMilliseconds(start, Stopwatch.GetTimestamp());
 
-                _responseBody = await GetBodyAsync(httpContext.Response);
-                _responseBody = string.IsNullOrWhiteSpace(_responseBody) ? null : _responseBody;
+                var responseBody = await GetBodyAsync(httpContext.Response);
 
-                GetLoggerWithContext(httpContext, elapsedMs).Information(MessageTemplate,
-                    GetRequestScheme(httpContext), httpContext.Request.Method, GetPath(httpContext),
-                    httpContext.Response?.StatusCode, elapsedMs); ;
+                GetLoggerWithContext(httpContext, requestBody, responseBody, elapsedMs)
+                    .Information(MessageTemplate, GetRequestScheme(httpContext),
+                        httpContext.Request.Method, GetPath(httpContext),
+                        httpContext.Response?.StatusCode, elapsedMs);
 
-                await responseBody.CopyToAsync(originalResponseBodyStream);
+                await responseBodyStream.CopyToAsync(originalResponseBodyStream);
             }
             catch (Exception ex)
             {
@@ -62,21 +57,21 @@ namespace WebAPI_DotNetCore_Demo.Middlewares
 
                 if (!env.IsDevelopment()) { await ProcessHttpResponseForException(httpContext, ex); }
 
-                _responseBody = await GetBodyAsync(httpContext.Response);
-                _responseBody = string.IsNullOrWhiteSpace(_responseBody) ? null : _responseBody;
+                var responseBody = await GetBodyAsync(httpContext.Response);
 
-                GetLoggerWithContext(httpContext, elapsedMs).Error(ex, MessageTemplate,
-                    GetRequestScheme(httpContext), httpContext.Request.Method, GetPath(httpContext),
-                    httpContext.Response?.StatusCode, elapsedMs);
+                GetLoggerWithContext(httpContext, requestBody, responseBody, elapsedMs)
+                    .Error(ex, MessageTemplate, GetRequestScheme(httpContext),
+                        httpContext.Request.Method, GetPath(httpContext),
+                        httpContext.Response?.StatusCode, elapsedMs);
 
-                await responseBody.CopyToAsync(originalResponseBodyStream);
+                await responseBodyStream.CopyToAsync(originalResponseBodyStream);
 
                 // Duplicate logs may appear in the log table due to rethrows.
                 if (env.IsDevelopment()) { throw; }
             }
             finally
             {
-                responseBody.Dispose();
+                responseBodyStream.Dispose();
                 httpContext.Response.Body = originalResponseBodyStream;
             }
         }
@@ -108,14 +103,15 @@ namespace WebAPI_DotNetCore_Demo.Middlewares
             return httpContext.Request.IsHttps ? "HTTPS" : "HTTP";
         }
 
-        private ILogger GetLoggerWithContext(HttpContext httpContext, double elapsedMs)
+        private static ILogger GetLoggerWithContext(HttpContext httpContext,
+            string requestBody, string responseBody, double elapsedMs)
         {
             return Log.ForContext<SerilogMiddleware>()
                 .ForContext("RequestMethod", httpContext.Request.Method)
                 .ForContext("RequestPath", GetPath(httpContext))
-                .ForContext("RequestBody", _requestBody, true)
+                .ForContext("RequestBody", requestBody, true)
                 .ForContext("ResponseStatusCode", httpContext.Response.StatusCode)
-                .ForContext("ResponseBody", _responseBody, true)
+                .ForContext("ResponseBody", responseBody, true)
                 .ForContext("ElapsedMs", elapsedMs)
                 .ForContext("UserName", httpContext.User.Identity.Name);
         }
@@ -134,27 +130,24 @@ namespace WebAPI_DotNetCore_Demo.Middlewares
         {
             request.EnableBuffering();
 
-            var buffer = new byte[Convert.ToInt32(request.ContentLength)];
+            using var streamReader = new StreamReader(request.Body, leaveOpen: true);
+            var requestBody = await streamReader.ReadToEndAsync();
 
-            await request.Body.ReadAsync(buffer, 0, buffer.Length);
+            _ = request.Body.Seek(0, SeekOrigin.Begin);
 
-            var bodyAsText = Encoding.UTF8.GetString(buffer);
-
-            request.Body.Position = 0;
-
-            return bodyAsText;
+            return string.IsNullOrWhiteSpace(requestBody) ? null : requestBody;
         }
 
         private static async Task<string> GetBodyAsync(HttpResponse response)
         {
-            response.Body.Seek(0, SeekOrigin.Begin);
+            _ = response.Body.Seek(0, SeekOrigin.Begin);
 
-            var streamReader = new StreamReader(response.Body);
-            var body = await streamReader.ReadToEndAsync();
+            using var streamReader = new StreamReader(response.Body, leaveOpen: true);
+            var responseBody = await streamReader.ReadToEndAsync();
 
-            response.Body.Seek(0, SeekOrigin.Begin);
+            _ = response.Body.Seek(0, SeekOrigin.Begin);
 
-            return body;
+            return string.IsNullOrWhiteSpace(responseBody) ? null : responseBody;
         }
     }
 }
